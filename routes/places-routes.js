974742cf074c761992994c09b1268860677fc45c
jayshreebawankar <98347627+jayshreebawankar@users.noreply.github.com>
2022-01-27 12:35:20 +0530

    i have update routes folder successfully

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,13 +1,11 @@
 const express = require('express');
-
-const placesControllers = require('../controllers/places-controller');
-
 const router = express.Router();
+const placesControllers = require('../controllers/places-controller');
 
 router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/:user/:uid', placesControllers.getPlaceByUserId);
 
-router.post('/', placesControllers.createPlace);
+// router.post('/', placesControllers.createPlace);
 
 module.exports = router;
