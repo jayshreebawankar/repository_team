cf6418712034570e70b18a0e899c2de099619e24
jayshreebawankar <98347627+jayshreebawankar@users.noreply.github.com>
2022-01-27 13:17:17 +0530

    i have been commiting places-controller

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -1,13 +1,10 @@
-const res = require('express/lib/response');
-const { restart } = require('nodemon');
-const uuid = require('uuid/v4');
-
+const express = require('express');
 const HttpError = require('../models/http-error');
 
 const DUMMY_PLACES = [
     {
         id: 'p1',
-        tittle: 'Empire State Building',
+        title: 'Empire State Building',
         decription: 'One of the most famous sky scrapers in the world!',
         location: {
             lat: 40.7484474,
@@ -15,38 +12,55 @@ const DUMMY_PLACES = [
         },
         address: '20 W 34th st, New York, NY 10001',
         creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        decription: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th st, New York, NY 10001',
+        creator: 'u2'
+    },
+    {
+        id: 'p3',
+        title: 'Empire State Building',
+        decription: 'One of the most famous sky scrapers in the world!',
+        location: {
+            lat: 40.7484474,
+            lng: -73.9871516
+        },
+        address: '20 W 34th st, New York, NY 10001',
+        creator: 'u3'
     }
 ];
 
-const getPlaceById = (req, res, next) => {
+const getPlaceById = ((req, res, next) => {
     const placedId =req.params.pid;
-
     const place =DUMMY_PLACES.find(p =>{
-        return p.id === placedId;
+        return p.id == placedId || p.title == placedId || p.description == placedId || p.location == placedId || p.address == placedId || p.creator == placedId;
     });
 
     if(!place) {
        throw new HttpError('Could not find a a place for a provided id.', 404);
     }
+    res.json({place})
+});
 
-      res.json({ place })
-    };
-
-    const getPlaceByUserId = (req, res, next) =>{
-        const userId = req.params.uid;
-
-
+const getPlaceByUserId = (req, res, next) =>{
+    const userId = req.params.uid;
     const place = DUMMY_PLACES.find(P =>{
-        return P.creator === userId;    
+        return P.creator == userId;    
     });
 
     if (!place) {
         return next(
             new HttpError('Could not find a a place for a provided id.', 404)
         );
-      }
-
-      res.json({ place });
+    }
+    res.json({ place });
 };
 
 const createPlace = (req, res, next) => {
